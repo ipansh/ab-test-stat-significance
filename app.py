11eb25252cab9d90eb2d0b086b1f0c34772a5896
Ilya <ilya.panshenskov@italki.com>
2021-12-15 15:00:16 +0800

    styling and fixes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from scipy.stats import norm
 app = Flask(__name__)
 
 def get_pvalue_student(mean_control, std_control, nobs_control, mean_test, std_test, nobs_test):
-    return round(stats.ttest_ind_from_stats(mean_control, std_control, nobs_control, mean_test, std_test, nobs_test, equal_var = True)[1],3)
+    return format(stats.ttest_ind_from_stats(mean_control, std_control, nobs_control, mean_test, std_test, nobs_test)[1],'.4f')
 
 def get_pvalue_conversion(control_size,control_conversion,experiment_size,experiment_conversion):
 
@@ -36,7 +36,15 @@ def get_pvalue_conversion(control_size,control_conversion,experiment_size,experi
 
 @app.route("/")
 def home():
-    return render_template('page.html') 
+    return render_template('two_sample_proportion.html')
+
+@app.route("/two_sample_proportion")
+def two_sample_proportion_page():
+    return render_template('two_sample_proportion.html')
+
+@app.route("/student_ttest")
+def student_ttest_page():
+    return render_template('student_ttest.html')
 
 @app.route("/pvaluestudent", methods = ['POST'])
 def pvalue_student_page():
@@ -52,7 +60,7 @@ def pvalue_student_page():
 
     pvalue = get_pvalue_student(mean_control, std_control, nobs_control, mean_test, std_test, nobs_test)
 
-    return render_template('page.html', prediction_text1 = 'P-value is: {}'.format(str(pvalue)))
+    return render_template('student_ttest.html', prediction_text1 = 'P-value is: {}'.format(str(pvalue)))
 
 @app.route("/pvalueconversion", methods = ['POST'])
 def pvalue_conversion_page():
@@ -64,9 +72,17 @@ def pvalue_conversion_page():
     numerator_test = int(message[2])
     denominator_test = int(message[3])    
 
-    pvalue = get_pvalue_conversion(denominator_control, numerator_control, numerator_test, denominator_test)
+    pvalue = get_pvalue_conversion(denominator_control, numerator_control, denominator_test, numerator_test)
+    pvalue = float(pvalue)
+    pvalue = round(pvalue,4)
 
-    return render_template('page.html', prediction_text2 = 'P-value is: {}'.format(str(pvalue)))
+    if pvalue < 0.05:
+      pvalue_explanation = 'is statistically significant and you can reject the null hypothsis.'
+    else:
+      pvalue_explanation = 'is not statistically significant and you can accept the null hypothsis.'
+
+
+    return render_template('two_sample_proportion.html', prediction_text2 = 'P-value is: {}, which means that result {}'.format(str(pvalue), pvalue_explanation))
 
 if __name__  == '__main__': 
     app.run(debug=True)
